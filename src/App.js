c38e1c148cdd2a30e3bba3f5eb56d411976e598c
jacky.leung <jacky.leung@oocl.com>
2018-12-06 14:27:36 +0800

    [LEUNGJA]feat: pages with nav, not found page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/first">First</Link>
+              </li>
+              <li>
+                <Link to="/second">Second</Link>
+              </li>
+            </ul>
+          </nav>
+
+<hr></hr>
+          <Switch>
+            <Route path="/" exact component={() => <h1>HOME</h1>} />
+            <Route path="/first" exact component={() => <h1>FIRST</h1>} />
+            <Route path="/second" exact component={() => <h1>SECOND</h1>} />
+            <Route exact component={() => <h1>404 page not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
